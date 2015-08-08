74ab574229c450ce3a8008e7220eedec0e622017
mzuralski <mzuralski@icloud.com>
2015-08-08 19:04:26 +0200

    First commit

--- First/FData/src/main/java/pl/first/fdata/Singleton.java
+++ First/FData/src/main/java/pl/first/fdata/Singleton.java
@@ -1,15 +1,34 @@
 package pl.first.fdata;
 
+import org.slf4j.Logger;
+
 /**
  * Created by Marcin on 2015-08-02.
  */
 public class Singleton {
-    private static Singleton ourInstance = new Singleton();
+
+
+    private static Singleton instance = new Singleton();
 
     public static Singleton getInstance() {
-        return ourInstance;
+        return instance;
     }
 
+    private String cos;
+
     private Singleton() {
+
+    }
+
+    public Singleton(String cos) {
+        this.cos = cos;
+    }
+
+    public String getCos() {
+        return cos;
+    }
+
+    public void setCos(String cos) {
+        this.cos = cos;
     }
 }
